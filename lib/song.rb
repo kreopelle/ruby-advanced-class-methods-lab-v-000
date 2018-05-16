0b3e0fe7255f25f6363aed9f86b38992a6332334
Kayla Reopelle <kreopelle@gmail.com>
2018-05-16 19:43:01 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,4 +33,12 @@ class Song
     self.all.detect{|song| song.name == name}
   end
 
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name)
+      self
+    else
+      self.create_by_name(name)
+    end
+  end
+
 end
