ca610ed3a09c1672676db3f0726c9c366048695d
Kayla Reopelle <kreopelle@gmail.com>
2018-05-16 19:48:34 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,10 +34,10 @@ class Song
   end
 
   def self.find_or_create_by_name(name)
-    if self.find_by_name(name) == nil
-      self.create_by_name(name)
-    else
+    if self.find_by_name(name)
       self
+    else
+      self.create_by_name(name)
     end
   end
 
