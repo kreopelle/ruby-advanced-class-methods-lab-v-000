880d0e9b7e775ec43e525ebd71b840c8a9af7863
Kayla Reopelle <kreopelle@gmail.com>
2018-05-16 20:20:23 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -49,6 +49,11 @@ class Song
     data = filename.split(" - ")
     artist_name = data[0]
     name = data[1].chomp(".mp3")
+    song = self.new
+    song.name = name
+    song.artist_name = artist_name
+    song
+  end 
 
 
 end
